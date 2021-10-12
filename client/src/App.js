0cfe868a0ac1d73327283e5f29af275618070624
Santiago Trini <santiagotrini@gmail.com>
2021-10-12 19:42:50 -0300

    deploy para Heroku

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,23 @@
-const App = () => <h1>App</h1>;
+import { useState } from 'react';
+const App = () => {
+  const [numero, setNumero] = useState(0);
+  const dec = () => {
+    setNumero (prev => prev - 1);
+    }
+  const inc = () => {
+    setNumero (prev => prev + 1);
+  }
+  const reset = () => {
+    setNumero (0);
+  }
+  return (
+    <div>
+      <h1>{numero}</h1>
+      <button onClick={dec}>-</button>
+      <button onClick={inc}>+</button>
+      <button onClick={reset}>resetiar</button>
+    </div>
+  );
+}
 
 export default App;
